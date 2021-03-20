ad5c692ec54974ac0d11ea8f0f8b1b0adff64b57
Richard Obiri <obiriaframson@gmail.com>
2021-03-20 00:44:37 +0000

    almost

--- server.js
+++ server.js
@@ -1,8 +1,19 @@
 const express = require("express")
-const http = require("http")
-const app = express()
-const server = http.createServer(app)
-const io = require("socket.io")(server)
+const INDEX = '/index.html';
+// const http = require("http")
+// const app = express()
+const PORT = process.env.PORT || 5000;
+const server = express()
+.use((req, res) => res.sendFile(INDEX, { root: __dirname }))
+.listen(PORT, () => console.log(`Listening on ${PORT}`));
+
+
+const io = require("socket.io")(server, {
+	cors: {
+		origin: "http://localhost:3000",
+		methods: [ "GET", "POST" ]
+	}
+})
 
 io.on("connection", (socket) => {
 	socket.emit("me", socket.id)
@@ -20,4 +31,4 @@ io.on("connection", (socket) => {
 	})
 })
 
-server.listen(5000, () => console.log("server is running on port 5000"))
+// server.listen(5000, () => console.log("server is running on port 5000"))
