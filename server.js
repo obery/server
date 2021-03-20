40462e6080c53b5480cc802da685c912d0bf32db
Richard Obiri <obiriaframson@gmail.com>
2021-03-20 17:09:03 +0000

    ss

--- server.js
+++ server.js
@@ -1,50 +1,34 @@
-const path = require("path");
-const http = require("http");
-const express = require("express");
-const socketio = require("socket.io");
-
-const app = express();
-const server = http.createServer(app);
-const io = socketio(server);
-
-const publicDirectoryPath = path.join(__dirname, "./public/");
-
-app.use(express.static(publicDirectoryPath));
-
-// .use((req, res) => res.sendFile(INDEX, { root: __dirname }))
-// .listen(PORT, () => console.log(`Listening on ${PORT}`));
-
-socketio(server, {
-	cors: {
-		origin: "https://avivid-client.herokuapp.com",
-		methods: ["GET", "POST"],
-		credentials: true,
-	},
-});
-
-
-io.on("connection", socket => {
-	socket.emit("me", socket.id);
-
-
-	socket.on("disconnect", () => {
-		socket.broadcast.emit("callEnded");
-	});
-
-	socket.on("callUser", data => {
-		io.to(data.userToCall).emit("callUser", {
-			signal: data.signalData,
-			from: data.from,
-			name: data.name,
-		});
-	});
-	socket.on("answerCall", data => {
-		io.to(data.to).emit("callAccepted", data.signal);
-	});
-});
+const express = require("express")
+const INDEX = '/index.html';
 
 const PORT = process.env.PORT || 5000;
+const server = express()
+.use((req, res) => res.sendFile(INDEX, { root: __dirname }))
+.listen(PORT, () => console.log(`Listening on ${PORT}`));
 
-server.listen(PORT, () => console.log("server is running on port 5000"));
+
+const io = require("socket.io")(server, {
+	cors: {
+		origin: "https://avivid-client.herokuapp.com",
+		methods: [ "GET", "POST" ],
+        credentials: true
+	}
+})
+
+io.on("connection", (socket) => {
+	socket.emit("me", socket.id)
+
+	socket.on("disconnect", () => {
+		socket.broadcast.emit("callEnded")
+	})
+
+	socket.on("callUser", (data) => {
+		io.to(data.userToCall).emit("callUser", { signal: data.signalData, from: data.from, name: data.name })
+	})
+
+	socket.on("answerCall", (data) => {
+		io.to(data.to).emit("callAccepted", data.signal)
+	})
+})
 
 // server.listen(5000, () => console.log("server is running on port 5000"))
